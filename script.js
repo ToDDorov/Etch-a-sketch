42ea88ed51eb4e897c266b7baf87549431850a0e
Todor <todorgz7@gmail.com>
2020-02-11 23:23:44 +0200

    Add darken color on mouse hover

--- script.js
+++ script.js
@@ -23,10 +23,47 @@ function createGrid(size) {
         for (let j = 0; j < size; j++) {
             let square = document.createElement('div');
             square.className = "square";
-            square.addEventListener('mouseover', () => {
-                square.classList.add("hover");
-            });
+            square.setAttribute("data-color", "");
+            square.addEventListener('mouseover', onSquareHover);
             row.appendChild(square);
         }
     }
+}
+
+function onSquareHover(e) {
+    let square = e.target;
+    let color;
+
+    if (square.dataset.color != "") {
+        color = darkenColor(square.dataset.color);
+    } else {
+        color = getRandomHSLColor();
+    }
+
+    square.style.backgroundColor = color;
+    square.setAttribute("data-color", color);
+}
+
+function getRandomHSLColor() {
+    const hue = getRandomRange(0, 360);
+    const sat = getRandomRange(0, 100);
+    const light = getRandomRange(0, 100);
+
+    return `hsl(${hue}, ${sat}%, ${light}%)`;
+}
+
+function getRandomRange(min, max) {
+    return Math.floor(Math.random() * (1 + max - min)) + min;
+}
+
+function darkenColor(color) {
+    const re = /(hsl\(\d{1,3}, \d{1,3}%, )(\d{1,3})%\)/;
+    let light = parseInt(color.match(re)[2]);
+
+    if (light - 10 > 0) light -= 10;
+    else light = 0;
+
+    let darkendedColor = color.replace(re, `$1${light}%)`);
+
+    return darkendedColor;
 }
